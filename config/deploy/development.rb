a99919af4520c634de2dbb3231a32ddd5d9a9021
kozawa <kozawa@willway.jp>
2016-09-08 15:56:21 +0900

    user change

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,11 +1,11 @@
 set :stage, :development #環境名
 #各サーバの役割を記述
-role :app, %w{kozawa@192.168.116.131} #アプリケーションサーバ
-role :web, %w{kozawa@192.168.116.131} #webサーバ
-role :db,  %w{kozawa@192.168.116.131} #DBサーバ
+role :app, %w{root@192.168.116.131} #アプリケーションサーバ
+role :web, %w{root@192.168.116.131} #webサーバ
+role :db,  %w{root@192.168.116.131} #DBサーバ
 #サーバ情報記述
 server '192.168.116.131', #サーバ名
-user: 'kozawa', #実行ユーザ
+user: 'root', #実行ユーザ
 roles: %w{web app db}, # サーバの役割
 ssh_options: {
     keys: %w(~/.ssh/id_rsa),
