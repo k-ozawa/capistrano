29df6e6055ecfe9e8453babb49edd6632ee4be83
k-ozawa <k-zawa@willway.jp>
2016-11-08 11:10:54 +0900

    test

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,14 +1,14 @@
 set :stage, :development #環境名
 #各サーバの役割を記述
-role :app, %w{ozawa@192.168.237.76} #アプリケーションサーバ
-role :web, %w{ozawa@192.168.237.76} #webサーバ
-role :db,  %w{ozawa@192.168.237.76} #DBサーバ
+role :app, %w{user@192.168.000.000} #アプリケーションサーバ
+role :web, %w{user@192.168.000.000} #webサーバ
+role :db,  %w{user@192.168.000.000} #DBサーバ
 #サーバ情報記述
-server '192.168.237.76', #サーバ名
-user: 'ozawa', #実行ユーザ
+server '192.168.000.000', #サーバ名
+user: 'user', #実行ユーザ
 roles: %w{web app db}, # サーバの役割
 ssh_options: {
     keys: %w(~/.ssh/id_rsa),
     #auth_methods: %w(publickey), # 認証方法 passwordも可能
-    password: 'ozawa' #password指定
+    password: 'password' #password指定
 }
